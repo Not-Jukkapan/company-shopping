e3adb249edf6a31d7f65e68fae13f18655a52d99
Jukkapan Nextzy <jukkapan.k@nextzy.com>
2024-12-11 11:24:13 +0700

    use yarn

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,23 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
-    .setTitle('Industrial Solutions API')
-    .setDescription('The Industrial Solutions API description')
-    .setVersion('1.0')
+    .setTitle("Industrial Solutions API")
+    .setDescription("The Industrial Solutions API description")
+    .setVersion("1.0")
     .addBearerAuth()
     .build();
-    
+
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
